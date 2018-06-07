096eb1ce6b7051dc4a15edad2b211d7ec87a617e
LeGa14 <glemonjr68@gmail.com>
2018-06-07 17:54:35 -0400

    user index route added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const userRouter = require('./controllers/user');
 
 const app = express();
 require('dotenv').config();
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(dotenv.config());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+
+// INDEX USER
+router.get('/', (req, res) => {
+    
+    User
+    .find()
+    .then((listOfUsers) => {
+        res.render('user/index', {
+            listOfUsers
+        })
+    })
+    .catch((err) => res.send(err))
+})
+
+// NEW USER
+// CREATE USER
+// SHOW USER
+// EDIT USER
+// UPDATE USER
+// DESTROY USER
